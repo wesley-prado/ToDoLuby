167ddb4666841ff4c8e65a1e4a494a85761cfebc
Wesley Prado <wprado1424@gmail.com>
2020-08-26 14:08:38 -0300

    --amend

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,26 +1,25 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Input, Title, Button } from '../../shared/styles'
-import { HomeSubtitle, HomeContainer, HomeLogo, HomeText } from './style'
+import React,{useState} from 'react';
+import {useHistory} from 'react-router-dom'
+import {Input, Button, Title} from '../../shared/styles'
+import {HomeContainer,HomeLogo,HomeSubtitle,HomeText} from './style'
 
-const Home: React.FC = () => {
+const Home: React.FC = ()=>{
   const [username, setUsername] = useState('')
   const history = useHistory()
-  const handleSubmit = () => {
-    localStorage.setItem('username', username)
-    return history.push('/todo')
+  function handleChange(e:React.ChangeEvent<HTMLInputElement>): void{
+    setUsername(e.target.value)
   }
-
+  function handleSubmit(): void{
+    localStorage.setItem('username', JSON.stringify(username))
+    history.push('/todo')
+  }
   return (
     <HomeContainer>
-      <HomeLogo />
+      <HomeLogo/>
       <Title>ToDoLuby</Title>
-      <HomeSubtitle>
-        A simple To Do List to organize what you have to accomplish during the
-        day.
-      </HomeSubtitle>
+      <HomeSubtitle>A simple To Do List to organize the tasks you have to accomplish during the day.</HomeSubtitle>
       <HomeText>Create a profile</HomeText>
-      <Input type="text" onChange={(e) => setUsername(e.target.value)} />
+      <Input type="text" onChange={handleChange}/>
       <Button onClick={handleSubmit}>Let's go!</Button>
     </HomeContainer>
   )
